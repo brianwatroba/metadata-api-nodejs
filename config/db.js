d19ddba2569e299e5c972c39aef1f2669f30df91
brian watroba <brianwatroba@gmail.com>
2022-09-24 14:13:41 -0400

    added troubleshooting for mongouri

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const { MONGO_URI } = require("./constants");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI", MONGO_URI);
     await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
